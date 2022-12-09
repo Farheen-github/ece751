d7b2a3a5f32e4c554d41e846f5436d5742d82fba
TheKalty <47836227+TheKalty@users.noreply.github.com>
2022-12-08 22:58:13 -0600

    Add files via upload
    FUN BUG FIXES

--- all_multipliers/Mult_tb.v
+++ all_multipliers/Mult_tb.v
@@ -6,6 +6,12 @@ wire [31:0] result, result_approx, result_drum, result_foil;
 
 reg clk = 1'b1;
 
+integer fd;
+integer wd0, wd1, wd2, wd3;
+integer i;
+reg [31:0] in1;
+reg [31:0] in2;
+reg [31:0] in3;
 
 Mult_all DUT(.a_operand(a_operand),.b_operand(b_operand),.Exception(Exception),
 		.Overflow(Overflow),.Underflow(Underflow),.Overflow_approx(Overflow_approx),.Underflow_approx(Underflow_approx), 
@@ -18,23 +24,28 @@ always clk = #5 ~clk;
 
 initial
 begin
-iteration (32'h4234_851F,32'h427C_851F,1'b0,1'b0,1'b0,32'h4532_10E9,`__LINE__); // 45.13 * 63.13 = 2849.0569;
+// Open file
+fd = $fopen("read.txt","r");
+wd0 = $fopen("perf.txt","w");
+wd1 = $fopen("aprrox.txt","w");
+wd2 = $fopen("drum.txt","w");
+wd3 = $fopen("foil.txt","w");
 
-iteration (32'h4049_999A,32'hC166_3D71,1'b0,1'b0,1'b0,32'hC235_5062,`__LINE__); //3.15 * -14.39 = -45.3285
-
-iteration (32'hC152_6666,32'hC240_A3D7,1'b0,1'b0,1'b0,32'h441E_5375,`__LINE__); //-13.15 * -48.16 = 633.304
-
-iteration (32'h4580_0000,32'h4580_0000,1'b0,1'b0,1'b0,32'h4B80_0000,`__LINE__); //4096 * 4096 = 16777216
-
-iteration (32'h3ACA_62C1,32'h3ACA_62C1,1'b0,1'b0,1'b0,32'h361F_FFE7,`__LINE__); //0.00154408081 * 0.00154408081 = 0.00000238418
-
-iteration (32'h0000_0000,32'h0000_0000,1'b0,1'b0,1'b0,32'h0000_0000,`__LINE__); // 0 * 0 = 0;
-
-iteration (32'hC152_6666,32'h0000_0000,1'b0,1'b0,1'b0,32'h441E_5375,`__LINE__); //-13.15 * 0 = 0;
-
-iteration (32'h7F80_0000,32'h7F80_0000,1'b1,1'b1,1'b0,32'h0000_0000,`__LINE__); 
-
-iteration (32'h0080_0000,32'h00180_000,1'b0,1'b0,1'b1,32'h0000_0000,`__LINE__);
+for (i = 0; i < 10000; i = i + 1) begin
+	$fscanf(fd, "%b\n", in1);
+	$fscanf(fd, "%b\n", in2);
+	$fscanf(fd, "%b\n", in3);
+	
+	// Check
+	iteration (in1, in2, in3, i);
+	
+	// write Outputs
+	$fdisplay(wd0,"%b", result);
+	$fdisplay(wd1,"%b", result_approx);
+	$fdisplay(wd2,"%b", result_drum);
+	$fdisplay(wd3,"%b", result_foil);
+	
+end
 
 $stop;
 
@@ -42,7 +53,6 @@ end
 
 task iteration(
 input [31:0] operand_a,operand_b,
-input Expected_Exception,Expected_Overflow,Expected_Underflow,
 input [31:0] Expected_result,
 input integer linenum 
 );
@@ -53,12 +63,8 @@ begin
 	b_operand = operand_b;
 end
 
-@(posedge clk)
-begin
-if ((Expected_result == result) && (Expected_Exception == Exception) && (Expected_Overflow == Overflow) && (Expected_Underflow == Underflow))
-	$display ("Test Passed - %d",linenum);
-$display ("Expected_result = %h, Result = %h, Approx-Result = %h, DRUM_result = %h, FOIL_Result = %h, \n Expected_Exception = %d, Exception = %d,\n Expected_Overflow = %d, Overflow = %d, Approx-Overflow = %d, DRUM_Overflow = %d, FOIL_Overflow = %d, \n Expected_Underflow = %d, Underflow = %d, Approx-Underflow = %d, DRUM_Underflow = %d, FOIL_Underflow = %d - %d \n ",Expected_result,result,result_approx,result_drum, result_foil,Expected_Exception,Exception,Expected_Overflow,Overflow,Overflow_approx,Overflow_drum, Overflow_foil, Expected_Underflow,Underflow,Underflow_approx,Underflow_drum, Underflow_foil, linenum);
-end
+@(posedge clk);
+
 end
 endtask
 endmodule
